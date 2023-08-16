fdffc4cb5e336e0a46a1f8c4f5e79e6732066337
you-25ssef <142083705+you-25ssef@users.noreply.github.com>
2023-08-16 13:40:27 +0300

    Add files via upload

---
+++ src/components/Header.vue
@@ -0,0 +1,42 @@
+<template>
+    <header>
+        <h1>
+            {{title}}
+            <Button 
+            v-show="homePage"
+            @btn-click="$emit('toggle-add-task')" :text="showAddTask ? 'close':'Add Task'" 
+            :color="showAddTask ? 'red' : 'green'"/>
+        </h1>
+    </header>
+</template>
+<script>
+import Button from './button.vue'
+export default {
+  name:'Header',
+  props:{
+    title: String,
+    showAddTask:Boolean,
+  },
+ components: {
+    Button,
+ },
+ computed:{
+    homePage(){
+        if(this.$route.path==='/'){
+            return true
+        }else{
+            return false
+        }
+    },
+ }
+}
+</script>
+<style scoped>
+header{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+}
+
+</style>
